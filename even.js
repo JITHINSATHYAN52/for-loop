84058bcb58dce7a3a8777bc8a9968fc92135e403
jithinsathyan <jithinsathyan8@gmail.com>
2025-08-07 07:49:08 +0530

    forloop

---
+++ even.js
@@ -0,0 +1,9 @@
+// print number from 1to 100.if a number is even print "even".else print"odd"
+
+
+for(let i=1;i<=100;i++){
+    if(i%2===0){console.log("print even")}
+ else {
+    console.log("print odd");
+}
+}
---
+++ fizzbuzz.js
@@ -0,0 +1,10 @@
+for(let i=1;i<=50;i++){
+ if(i%3==0 && i%5==0){console.log("fizzbuzz")
+
+ }
+
+ else if(i%3 ==0){console.log(i+"fizz")
+
+ }else if (i%5==0){
+    console.log(i="buzz")
+ }else{console.log(i)}}
